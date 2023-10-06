acd150f68f2a8481b9e8b8f2f5499cb6552e0c21
bence <benceluzsinszky@gmail.com>
2023-10-06 15:02:08 +0200

    Added documentation to Sprite.java

--- app/src/main/java/com/android/rockpaperscissors/Sprite.java
+++ app/src/main/java/com/android/rockpaperscissors/Sprite.java
@@ -14,40 +14,81 @@ public class Sprite {
     private Sprite closestPrey, closestHunter;
     private Sprite[] ownGroup, hunterGroup, preyGroup;
 
+    /**
+     * Create a single sprite.
+     * @param size          X,Y dimensions of the sprite.
+     * @param image         Bitmap used to draw the sprite.
+     * @param x             Spawning X coordinate.
+     * @param y             Spawning Y coordinate.
+     * @param ownGroup      Group of the sprite.
+     * @param hunterGroup   Hunter group of the sprite (the sprites that can consume it).
+     * @param preyGroup     Prey group for the sprite (the sprites it consumes).
+     * @param speed         Movement speed of the sprite.
+     */
     public Sprite(int size, Bitmap image, int x, int y, Sprite[] ownGroup, Sprite[] hunterGroup, Sprite[] preyGroup, int speed) {
         this.size = size;
         this.speed = speed;
-        screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-        int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
-        bottomWall = screenHeight - screenHeight /15;
-        this.image = image;
         this.x = x;
         this.y = y;
         this.ownGroup = ownGroup;
         this.hunterGroup = hunterGroup;
         this.preyGroup = preyGroup;
+        this.image = image;
+        screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+        int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+        bottomWall = screenHeight - screenHeight /15;
     }
 
+    /**
+     * Gets the coordinates of the sprite as an array of floats.
+     *
+     * @return An array containing the X and Y coordinates of the sprite.
+     */
     public float[] getCoordinates() {
         return new float[]{x, y};
     }
 
+    /**
+     * Sets the image for the sprite.
+     *
+     * @param image The Bitmap image to set for the sprite.
+     */
     public void setImage(Bitmap image) {
         this.image = image;
     }
 
+    /**
+     * Sets the group of sprites to which this sprite belongs.
+     *
+     * @param ownGroup The Sprite array representing the group of sprites to set.
+     */
     public void setOwnGroup(Sprite[] ownGroup) {
         this.ownGroup = ownGroup;
     }
 
+    /**
+     * Sets the group of sprites that are hunting this sprite.
+     *
+     * @param hunterGroup The Sprite array representing the group of hunting sprites to set.
+     */
     public void setHunterGroup(Sprite[] hunterGroup) {
         this.hunterGroup = hunterGroup;
     }
 
+    /**
+     * Sets the group of sprites that this sprite is hunting.
+     *
+     * @param preyGroup The Sprite array representing the group of prey sprites to set.
+     */
     public void setPreyGroup(Sprite[] preyGroup) {
         this.preyGroup = preyGroup;
     }
 
+    /**
+     * Draws the sprite's image onto the Canvas at the current (x, y) coordinates.
+     *
+     * @param canvas The Canvas on which to draw the sprite's image.
+     */
     public void draw(Canvas canvas) {
         canvas.drawBitmap(image, x, y, null);
     }
@@ -60,146 +101,39 @@ public class Sprite {
         eat();
     }
 
-    public void randomMovement() {
-        double randomX = ThreadLocalRandom.current().nextDouble(speed*-1, speed);
-        double randomY = ThreadLocalRandom.current().nextDouble(speed*-1, speed);
-        x += randomX;
-        y += randomY;
-    }
-
-    public void checkWalls() {
-        // sprite touches left wall
-        if (x <= 20) {
-            x = 20;
-        }
-        // sprite touches right wall
-        if (x + size >= screenWidth-20) {
-            x = screenWidth-20-size;
-        }
-        // sprite touches top wall
-        if (y <= 20) {
-            y = 20;
-        }
-        // sprite touches bottom wall
-        if (y + size >= bottomWall-20) {
-            y = bottomWall-20-size;
-        }
-    }
-
-    private void wallAvoidance(){
-        // Only avoid walls in the outer quarters
-        if (!(x+(size*0.5) < screenWidth*0.25 || x+(size*0.5) > screenWidth*0.75 ||
-                y+(size*0.5) < bottomWall*0.25 || y+(size*0.5) > bottomWall*0.75)) {
-            return;
-        }
-        double avoidanceWeight = 0.002;
-        int dirX = 1;
-        double avoidanceX = screenWidth * 0.5 - x + (size * 0.5);
-        int dirY = 1;
-        double avoidanceY = bottomWall * 0.5 - y + (size * 0.5);
-        if (x + (size * 0.5) >= screenWidth * 0.5) {
-            dirX = -1;
-            avoidanceX = x + (size * 0.5) - screenWidth * 0.5;
-        }
-        if (y + (size * 0.5) >= bottomWall * 0.5) {
-            dirY = -1;
-            avoidanceY = y + (size * 0.5) - bottomWall * 0.5;
-        }
-        x += dirX * avoidanceX * avoidanceWeight;
-        y += dirY * avoidanceY * avoidanceWeight;
-    }
-
+    /**
+     * Moves the sprite by performing random movement, hunting, and running behaviors.
+     * This method should be called to update the sprite's position.
+     */
     public void moveSprite() {
+        randomMovement();
         closestHunter = getInfo(hunterGroup);
         closestPrey = getInfo(preyGroup);
-        randomMovement();
         hunt();
         run();
     }
 
-    public void selfCollision() {
-        for (Sprite sprite : ownGroup) {
-            if (sprite == this || sprite == null) {
-                continue;
-            }
-            float[] spriteCoordinates = sprite.getCoordinates();
-            float distanceX = x - spriteCoordinates[0];
-            float distanceY = y - spriteCoordinates[1];
-            // if both distances are smaller then the Sprite size, then they are colliding
-            if (!(abs(distanceY) <= size && abs(distanceX) <= size)) {
-                continue;
-            }
-            // this on right
-            if (distanceX > 0) {
-                x += speed;
-            }
-            // this on left
-            else if (distanceX < 0) {
-                x -= speed;
-            }
-            // this on bottom
-            if (distanceY > 0) {
-                y += speed;
-            }
-            // this on top
-            else if (distanceY < 0) {
-                y -= speed;
-            }
-        }
+    /**
+     * Generates random movement for the sprite within its speed range.
+     */
+    public void randomMovement() {
+        double randomX = ThreadLocalRandom.current().nextDouble(speed * -1, speed);
+        double randomY = ThreadLocalRandom.current().nextDouble(speed * -1, speed);
+        x += randomX;
+        y += randomY;
     }
 
-    public void hunt(){
-        if(closestPrey != null){
-            double dX = (closestPrey.x + (size*0.5)- x);
-            double dY = (closestPrey.y + (size*0.5) - y);
-            double distance = Math.sqrt(Math.pow(dY, 2) + Math.pow(dX, 2));
-            x += (dX / distance) * speed;
-            y += (dY / distance) * speed;
-        }
-    }
-
-    public void run(){
-        // Run if hunter exists and closer than 500
-        if(closestHunter == null) {
-            return;
-        }
-        double dX = (closestHunter.x + (size*0.5) - x);
-        double dY = (closestHunter.y + (size*0.5) - y);
-        double distance = Math.sqrt(Math.pow(dY, 2) + Math.pow(dX, 2));
-        if(distance > 500) {
-            return;
-        }
-        x -= (dX / distance) * speed * 0.95;
-        y -= (dY / distance) * speed * 0.95;
-    }
-
-    public void eat(){
-        for(int i=0; i<preyGroup.length; i++){
-            Sprite sprite = preyGroup[i];
-            if(sprite == null) {
-                continue;
-            }
-            float[] spriteCoordinates = sprite.getCoordinates();
-            double distanceX = x - spriteCoordinates[0];
-            double distanceY = y - spriteCoordinates[1];
-            double hitBox = size*0.7;
-            if (!(abs(distanceY) <= hitBox && abs(distanceX) <= hitBox)) {
-                continue;
-            }
-            preyGroup[i] = null;
-            ownGroup[i] = sprite;
-            sprite.setImage(image);
-            sprite.setOwnGroup(ownGroup);
-            sprite.setHunterGroup(hunterGroup);
-            sprite.setPreyGroup(preyGroup);
-        }
-    }
-
-    public Sprite getInfo(Sprite[] group){
+    /**
+     * Finds and returns the closest sprite from a given group.
+     *
+     * @param group The group of sprites from which to find the closest sprite.
+     * @return The closest sprite in the group, or null if the group is empty or null.
+     */
+    public Sprite getInfo(Sprite[] group) {
         Sprite closestSprite = null;
         double closestDistance = Double.POSITIVE_INFINITY;
-        for(Sprite sprite: group){
-            if(sprite == this || sprite == null) {
+        for (Sprite sprite : group) {
+            if (sprite == this || sprite == null) {
                 continue;
             }
             float[] spriteCoordinates = sprite.getCoordinates();
@@ -211,5 +145,165 @@ public class Sprite {
             }
         }
         return closestSprite;
+    }
+
+    /**
+     * Moves the sprite towards the direction of its closest prey. TODO: make a single hunt-run method
+     */
+    public void hunt() {
+        if (closestPrey != null) {
+            double dX = (closestPrey.x + (size * 0.5) - x);
+            double dY = (closestPrey.y + (size * 0.5) - y);
+            double distance = Math.sqrt(Math.pow(dY, 2) + Math.pow(dX, 2));
+            x += (dX / distance) * speed;
+            y += (dY / distance) * speed;
+        }
+    }
+
+    /**
+     * Makes the sprite to run away from its closest hunter if it exists and is within a certain distance.
+     * The sprite tries to maintain a safe distance from the hunter.
+     */
+    public void run() {
+        if (closestHunter == null) {
+            return;
+        }
+        double dX = (closestHunter.x + (size * 0.5) - x);
+        double dY = (closestHunter.y + (size * 0.5) - y);
+        double distance = Math.sqrt(Math.pow(dY, 2) + Math.pow(dX, 2));
+        if (distance > 500) {
+            return;
+        }
+        x -= (dX / distance) * speed * 0.95;
+        y -= (dY / distance) * speed * 0.95;
+    }
+
+    /**
+     * This method prevents the sprite from moving outside the visible area.
+     * If the sprite touches the left, right, top, or bottom wall of the screen,
+     * its position is adjusted accordingly.
+     */
+    public void checkWalls() {
+        // Check and adjust for the left wall
+        if (x <= 20) {
+            x = 20;
+        }
+
+        // Check and adjust for the right wall
+        if (x + size >= screenWidth - 20) {
+            x = screenWidth - 20 - size;
+        }
+
+        // Check and adjust for the top wall
+        if (y <= 20) {
+            y = 20;
+        }
+
+        // Check and adjust for the bottom wall
+        if (y + size >= bottomWall - 20) {
+            y = bottomWall - 20 - size;
+        }
+    }
+
+    /**
+     * Applies wall avoidance behavior to the sprite's movement so it does not stick on the screen boundaries.
+     * The method uses {@code avoidanceWeight} to control the amount of avoidance.
+     */
+    private void wallAvoidance() {
+        // Only avoid walls in the outer quarters
+        if (!(x + (size * 0.5) < screenWidth * 0.25 || x + (size * 0.5) > screenWidth * 0.75 ||
+                y + (size * 0.5) < bottomWall * 0.25 || y + (size * 0.5) > bottomWall * 0.75)) {
+            return;
+        }
+
+        // Define the avoidance weight
+        double avoidanceWeight = 0.002;
+
+        // Initialize direction flags for avoidance in X and Y directions
+        int dirX = 1;
+        double avoidanceX = screenWidth * 0.5 - x + (size * 0.5);
+        int dirY = 1;
+        double avoidanceY = bottomWall * 0.5 - y + (size * 0.5);
+
+        // Determine avoidance direction based on sprite's position
+        if (x + (size * 0.5) >= screenWidth * 0.5) {
+            dirX = -1;
+            avoidanceX = x + (size * 0.5) - screenWidth * 0.5;
+        }
+        if (y + (size * 0.5) >= bottomWall * 0.5) {
+            dirY = -1;
+            avoidanceY = y + (size * 0.5) - bottomWall * 0.5;
+        }
+
+        // Apply avoidance by adjusting sprite's position
+        x += dirX * avoidanceX * avoidanceWeight;
+        y += dirY * avoidanceY * avoidanceWeight;
+    }
+
+    /**
+     * Handles self-collision detection and resolution for the sprite within its own group.
+     * If the sprite collides with any other sprite in its own group, it adjusts its position
+     * to prevent overlap and maintain separation.
+     */
+    public void selfCollision() {
+        for (Sprite sprite : ownGroup) {
+            if (sprite == this || sprite == null) {
+                continue;
+            }
+            float[] spriteCoordinates = sprite.getCoordinates();
+            float distanceX = x - spriteCoordinates[0];
+            float distanceY = y - spriteCoordinates[1];
+
+            // Check if both distances are smaller than or equal to the sprite's size, indicating a collision
+            if (!(Math.abs(distanceY) <= size && Math.abs(distanceX) <= size)) {
+                continue;
+            }
+
+            // Handle collision resolution based on relative positions
+            if (distanceX > 0) {
+                x += speed; // Move to the right
+            } else if (distanceX < 0) {
+                x -= speed; // Move to the left
+            }
+
+            if (distanceY > 0) {
+                y += speed; // Move down
+            } else if (distanceY < 0) {
+                y -= speed; // Move up
+            }
+        }
+    }
+
+    /**
+     * Implements the eating behavior of the sprite, where it consumes prey sprites that are within its reach.
+     * If a prey sprite is consumed, it is removed from the prey group, and the sprite taking the prey's place
+     * becomes a part of the sprite's own group.
+     */
+    public void eat() {
+        for (int i = 0; i < preyGroup.length; i++) {
+            Sprite sprite = preyGroup[i];
+            if (sprite == null) {
+                continue;
+            }
+            float[] spriteCoordinates = sprite.getCoordinates();
+            double distanceX = x - spriteCoordinates[0];
+            double distanceY = y - spriteCoordinates[1];
+            double hitBox = size * 0.7;
+
+            // Check if both distances are smaller than or equal to the hitbox size, indicating successful eating
+            if (!(Math.abs(distanceY) <= hitBox && Math.abs(distanceX) <= hitBox)) {
+                continue;
+            }
+
+            // Remove the eaten prey sprite from the prey group
+            preyGroup[i] = null;
+
+            // Add the sprite to the sprite's own group and configure its properties
+            ownGroup[i] = sprite;
+            sprite.setImage(image);
+            sprite.setOwnGroup(ownGroup);
+            sprite.setHunterGroup(hunterGroup);
+            sprite.setPreyGroup(preyGroup);
+        }
     }
 }
